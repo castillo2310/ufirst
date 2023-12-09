18500a98be17161b7899d94f31a689e5b8f42de0
Cristian Castillo <ccastillo@eactivos.com>
2023-12-09 13:36:40 +0100

    Replaced App namespace with ufirst.

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use ufirst\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace ufirst;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
