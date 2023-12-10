8883018371e15eedd20b188b6791575943e6eb82
Cristian Castillo <ccastillo@eactivos.com>
2023-12-10 17:36:27 +0100

    Removed useless options in RequestMethod enum.

--- backend/src/Log/Domain/RequestMethod.php
+++ backend/src/Log/Domain/RequestMethod.php
@@ -7,10 +7,4 @@ enum RequestMethod: string
     case GET = 'GET';
     case HEAD = 'HEAD';
     case POST = 'POST';
-    case PUT = 'PUT';
-    case DELETE = 'DELETE';
-    case CONNECT = 'CONNECT';
-    case OPTIONS = 'OPTIONS';
-    case TRACE = 'TRACE';
-    case PATCH = 'PATCH';
 }
