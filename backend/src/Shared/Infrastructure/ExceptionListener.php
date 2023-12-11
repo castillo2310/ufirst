d777f7209e03d1cb1741cddb2f178c58caeb5903
Cristian Castillo <ccastillo@eactivos.com>
2023-12-11 19:43:27 +0100

    Injected logger into ExceptionListener to log all the exception messages.

--- backend/src/Shared/Infrastructure/ExceptionListener.php
+++ backend/src/Shared/Infrastructure/ExceptionListener.php
@@ -2,17 +2,24 @@
 
 namespace ufirst\Shared\Infrastructure;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
 #[AsEventListener(event: 'kernel.exception')]
-class ExceptionListener
+final readonly class ExceptionListener
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
+
+        $this->logger->error($exception->getMessage());
 
         $response = new JsonResponse(
             [
